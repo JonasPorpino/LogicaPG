c5600da27399423a92b0c76c75d59fb94d5c6c79
JonasPorpino <49802323+JonasPorpino@users.noreply.github.com>
2024-06-03 14:43:43 -0300

    teste

--- js/variaveis.js
+++ js/variaveis.js
@@ -2,10 +2,11 @@ function somar() {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
     var resultado = num1 + num2;
-    document.getElementById("resultado").innerHTML = resultado;
-    document.getElementById("v1").innerHTML = num1+' <- [ Variável 1 ]';
-    document.getElementById("v2").innerHTML = num2+' <- [ Variável 2 ]';
-    document.getElementById("v3").innerHTML = resultado+' <- [ Variável Resultado] ';
+    document.getElementById("v3").innerHTML = resultado;
+    document.getElementById("s").innerHTML = " ⇽ ";
+    document.getElementById("v1").innerHTML = num1;
+    document.getElementById("r").innerHTML = " + ";
+    document.getElementById("v2").innerHTML = num2;
 }
 
 function subtrair() {
